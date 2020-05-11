a158dd324565f875538d029c7ea4bea0d0d98445
Leamsi Escribano <leamsii.e@gmail.com>
2020-05-11 17:26:06 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,6 +1,7 @@
 def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
-  # keyword "return." Nice and neat.
+  # keyword "return." Nice and neat This type of return sometimes causes confusion among other.
+  # Type of programmers, I no like.
 
 end
 
